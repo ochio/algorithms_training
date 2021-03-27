6654b079a9639b1e63ac3103a21ef6ae52cce31a
ochi <ocos54ce@gmail.com>
2021-03-28 01:38:20 +0900

    ユーグリッドの互除法

---
+++ math/euclidean-algorithm/euclideanAlgorithm.js
@@ -0,0 +1,19 @@
+/**
+ * 再帰的に最大公約数を見つけるユーグリッドの互除法
+ * @param {number} originalA
+ * @param {number} originalB
+ * @return {number}
+ */
+
+function euclideanAlgorithm(originalA, originalB){
+// 引数の絶対値をとって自然数にする。
+	const a = Math.abs(originalA)
+	const b = Math.abs(originalB)
+
+	// 合同式を使ってるらしいけど何してるか分からん
+	console.log(a,"%",b, a%b);
+	return (b === 0) ? a : euclideanAlgorithm(b, a % b) 
+	// a < b だったら１回目で入れ替わる。a % b = a になるから。
+}
+
+console.log(euclideanAlgorithm(253,105))
