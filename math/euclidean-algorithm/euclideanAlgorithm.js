4aae0f9640ea111f4f630427aa4e1863a026a1cc
ochi <ocos54ce@gmail.com>
2021-03-28 01:38:47 +0900

    コメント

--- math/euclidean-algorithm/euclideanAlgorithm.js
+++ math/euclidean-algorithm/euclideanAlgorithm.js
@@ -5,6 +5,8 @@
  * @return {number}
  */
 
+
+// 余りを割っていくのなんで？
 function euclideanAlgorithm(originalA, originalB){
 // 引数の絶対値をとって自然数にする。
 	const a = Math.abs(originalA)
@@ -12,7 +14,7 @@ function euclideanAlgorithm(originalA, originalB){
 
 	// 合同式を使ってるらしいけど何してるか分からん
 	console.log(a,"%",b, a%b);
-	return (b === 0) ? a : euclideanAlgorithm(b, a % b) 
+	return (b === 0) ? a : euclideanAlgorithm(b, a % b) //→割る数を余りで割っていく
 	// a < b だったら１回目で入れ替わる。a % b = a になるから。
 }
 
